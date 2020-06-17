253e430dc8e292cf72bd345e51cb39cc27f2c0da
Mrityunjay <kmrityunjay@gmail.com>
2020-06-17 10:20:00 +0530

    Environment Update

--- overlay/usr/local/www/storagenode/environment.php
+++ overlay/usr/local/www/storagenode/environment.php
@@ -2,7 +2,8 @@
 	# ------------------------------------------------------------------------
 	#  Set environment variables
 	# ------------------------------------------------------------------------
-	$filename = "config.json";
+	$cfgfilename = "config.json";
+    $logfilename = "log.json";
 
 	$platformBase   = $_SERVER['DOCUMENT_ROOT'];
 	$moduleBase     = $platformBase . dirname($_SERVER['PHP_SELF']) ;
@@ -10,14 +11,15 @@
 	// $rootBase	= "/root/.local/share/storj/identity" ;
 
 
-	$file           = $moduleBase  . DIRECTORY_SEPARATOR . $filename  ;
+	$cfgfile        = $moduleBase  . DIRECTORY_SEPARATOR . $cfgfilename  ;
+    $logfile        = $moduleBase  . DIRECTORY_SEPARATOR . $logfilename  ;
 	$startScript    = $scriptsBase . DIRECTORY_SEPARATOR . 'storagenodestart.sh' ;
 	$stopScript     = $scriptsBase . DIRECTORY_SEPARATOR . 'storagenodestop.sh' ;
 	$updateScript	= $scriptsBase . DIRECTORY_SEPARATOR . 'storagenodeupdate.sh' ;
 	$checkScript    = $scriptsBase . DIRECTORY_SEPARATOR . 'checkStorj.sh' ;
 	$isRunning      = $scriptsBase . DIRECTORY_SEPARATOR . 'isRunning.sh' ;
 	$storageBinary  = $scriptsBase . DIRECTORY_SEPARATOR . 'storagenode' ;
-	$yamlPath	= $scriptsBase . DIRECTORY_SEPARATOR . 'docker-compose_base.yml' ;
+	$yamlPath	    = $scriptsBase . DIRECTORY_SEPARATOR . 'docker-compose_base.yml' ;
 
 	$identityFile   = $moduleBase  . DIRECTORY_SEPARATOR . 'identity.pid' ;
 
